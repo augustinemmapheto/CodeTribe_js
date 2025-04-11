9e2efda91a60af4a2584996533b0d27a5bb7cf12
augustine_dev <augustinemmapheto@gmail.com>
2025-04-11 07:43:08 +0200

    variable commit

---
+++ variable.js
@@ -0,0 +1,24 @@
+// Exercise 1: Variable Declaration
+let name = "Augustine";
+let age = 25;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+// Exercise 2: Variable Reassignment
+let favouriteColour = "black";
+console.log("My favourite color is:", favouriteColour);
+favouriteColour = "green";
+console.log("Now, my favourite color is:", favouriteColour);
+
+// Exercise 3: Arithmetic
+let num1 = 10;
+let num2 = 3;
+
+console.log("Sum:", num1 + num2);
+console.log("Difference:", num1 - num2);
+console.log("Product:", num1 * num2);
+console.log("Quotient:", num1 / num2);
+console.log("Remainder:", num1 % num2);
